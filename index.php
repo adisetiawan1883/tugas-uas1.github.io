21457b062477af295a8923e7d52f62ec9f2bd47b
adisetiawan1883 <123556193+adisetiawan1883@users.noreply.github.com>
2023-01-26 09:46:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Jual Sepatu Futsal</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>Sepatu Futsal</p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>copyright by  <a href="" target="_blank">Sans</a></p>
+  </div>
+
+</body>
+
+</html>
